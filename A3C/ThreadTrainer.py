f83af96d9b7a9d8f85f89dfb6feb269b7514ac6a
wmjones <wyattjo116@gmail.com>
2018-02-27 14:04:51 -0600

    added gitignore

--- A3C/ThreadTrainer.py
+++ A3C/ThreadTrainer.py
@@ -17,7 +17,7 @@ class ThreadTrainer(Thread):
         while not self.exit_flag:
             batch_size = 0
             while batch_size < Config.TRAINING_MIN_BATCH_SIZE:
-                x_, y_, a_, ora_, r_, orr_ = self.server.training_q.get()
+                x_, y_, a_, ora_, r_, orr_, idx_ = self.server.training_q.get()
                 if batch_size == 0:
                     x__ = np.asarray(x_)
                     y__ = np.asarray(y_)
@@ -25,6 +25,7 @@ class ThreadTrainer(Thread):
                     r__ = np.asarray([r_])  # not sure why this one needed brackets
                     ora__ = np.asarray(ora_)
                     orr__ = np.asarray([orr_])  # not sure why this one needed brackets
+                    idx__ = np.asarray(idx_)
                 else:
                     x__ = np.vstack((x__, x_))
                     y__ = np.vstack((y__, y_))
@@ -32,8 +33,9 @@ class ThreadTrainer(Thread):
                     r__ = np.vstack((r__, r_))
                     ora__ = np.vstack((ora__, ora_))
                     orr__ = np.vstack((orr__, orr_))
-                batch_size += x__.shape[0]
-            self.server.train_model(x__, y__, a__, ora__, r__, orr__, self.id)
+                    idx__ = np.vstack((idx__, idx_))
+                batch_size = x__.shape[0]
+            self.server.train_model(x__, y__, a__, ora__, r__, orr__, idx__.reshape(-1), self.id)
 
             #     print("state: ", x__)
             #     print("value: ", r__)
